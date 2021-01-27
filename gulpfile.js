77e40752ad6eef110c63248473de839a8cb96f68
jensicode <>
2021-01-27 20:09:46 +0100

    Added gulpfile with config + installed some packages

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+
+function copyFonts(cb) {
+    gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('wwwroot/assets/fonts'));
+    cb();
+}
+
+function sassToCss(cb) {
+    gulp.src('Content/css/*.scss')
+        .pipe(sass())
+        .pipe(cleanCSS({ compatibility: '*' }))
+        .pipe(gulp.dest('wwwroot/assets/css'));
+    cb();
+}
+
+exports.default = gulp.series(copyFonts, sassToCss);
