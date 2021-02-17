aef5ba92cf95dfba6b210261c10f0dc42ae2e8c1
jensicode <>
2021-02-17 19:13:49 +0100

    Added a Logout link with logic

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace batchmailr.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("/");
+        }
+    }
+}
