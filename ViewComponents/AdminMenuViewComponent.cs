60cbaf3616d4752628268d3a570d7b52fe01d97f
jensicode <>
2021-02-17 18:59:41 +0100

    Initial work on the AdminMenu component

---
+++ ViewComponents/AdminMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace batchmailr.ViewComponents
+{
+    public class AdminMenuViewComponent : ViewComponent
+    {
+        private SignInManager<IdentityUser> _signInManager;
+
+        public AdminMenuViewComponent(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            if (_signInManager.IsSignedIn(HttpContext.User))
+            {
+                return View();
+            }
+            return Content("");
+        }
+    }
+}
