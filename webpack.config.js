6af69e36ccabfe8c386c798834135dc4f3487f67
jensicode <>
2021-02-20 15:12:06 +0100

    Added webpack with config + installed some packages

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+﻿const webpack = require('webpack');
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    entry: {
+        basescripts: './Content/js/basescripts.js'
+    },
+    mode: "production",
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false
+            })
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'wwwroot/assets/js')
+    }
+}
