0111ce73897a0d7d771c5b59aecbe5d18f742a45
jensicode <>
2021-02-27 16:10:06 +0100

    Added some more controllers

---
+++ Controllers/LayoutTemplatesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using batchmailr.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace batchmailr.Controllers
+{
+    public class LayoutTemplatesController : Controller
+    {
+        private ILayoutTemplatesService _layoutTemplatesService;
+
+        public LayoutTemplatesController(ILayoutTemplatesService layoutTemplatesService)
+        {
+            _layoutTemplatesService = layoutTemplatesService;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var model = _layoutTemplatesService.GetAllLayoutTemplateItems();
+            return View(model);
+        }
+    }
+}
---
+++ Controllers/TextTemplatesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using batchmailr.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace batchmailr.Controllers
+{
+    public class TextTemplatesController : Controller
+    {
+        private ITextTemplatesService _textTemplatesService;
+
+        public TextTemplatesController(ITextTemplatesService textTemplatesService)
+        {
+            _textTemplatesService = textTemplatesService;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var model = _textTemplatesService.GetAllTextTemplateItems();
+            return View(model);
+        }
+    }
+}
