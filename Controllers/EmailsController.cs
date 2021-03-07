a0716687f0b2ce21db80654eec5b57c60b4b5a5f
jensicode <>
2021-03-07 23:59:57 +0100

    Added initial form for creating an e-mail

--- Controllers/EmailsController.cs
+++ Controllers/EmailsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using batchmailr.Services;
+using batchmailr.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,20 @@ namespace batchmailr.Controllers
         {
             var model = _emailsService.GetAllEmailItems();
             return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["pageTitle"] = "Create e-mail";
+            var model = new EmailsViewModel();
+            return View("CreateAndEdit", model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmailsViewModel model)
+        {
+            ViewData["pageTitle"] = "Create e-mail";
+            return View("CreateAndEdit", model);
         }
     }
 }
---
+++ ViewsModels/EmailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using batchmailr.Models;
+
+namespace batchmailr.ViewModels
+{
+    public class EmailsViewModel
+    {
+        public string EmailSubject { get; set; }
+
+        public string EmailHtmlContent { get; set; }
+    }
+}
