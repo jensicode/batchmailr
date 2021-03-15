99461ad4ba40dbb8dd17c56e2ffeabbc1dbff21f
jensicode <>
2021-03-15 20:38:59 +0100

    Added initial form for creating a text template

--- Controllers/TextTemplatesController.cs
+++ Controllers/TextTemplatesController.cs
@@ -2,6 +2,7 @@
 using batchmailr.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using batchmailr.ViewModels;
 
 namespace batchmailr.Controllers
 {
@@ -19,6 +20,20 @@ namespace batchmailr.Controllers
         {
             var model = _textTemplatesService.GetAllTextTemplateItems();
             return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["pageTitle"] = "Create text template";
+            var model = new TextTemplateViewModel();
+            return View("CreateAndEdit", model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TextTemplateViewModel model)
+        {
+            ViewData["pageTitle"] = "Create text template";
+            return View("CreateAndEdit", model);
         }
     }
 }
---
+++ ViewsModels/TextTemplateViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using batchmailr.Models;
+
+namespace batchmailr.ViewModels
+{
+    public class TextTemplateViewModel
+    {
+        public string TextTemplateName { get; set; }
+
+        public string TextTemplateContent { get; set; }
+    }
+}
